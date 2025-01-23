878ae22eeed6b9c8a5868d0a2a9d72b4962b07d0
JhonPoma <jpm11797@gmail.com>
2025-01-23 18:26:19 -0500

    cifrando las password con bcrypt, ruta get para mostrar todos los usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,36 @@
 import  {User}  from '../models/user.js'
+import bcrypt from 'bcrypt'
 
 const pruebaUser = (req, res)=>{
     return res.status(200).send({
         mesage:"msj enviado desde controlador/user.js"
     })
 }
+const getAllUsers = async (req, res)=>{
+
+    try {
+        const usuarios = await User.find()
+
+        return res.status(200).json({
+            status : 'success',
+            msj : 'Mostrando todos los usuario de la BD',
+            usuario : usuarios
+        })
+
+        
+    } catch (error) {
+        return res.status(500).json({
+            status : 'errorr',
+            msj : 'no pude traer todos los usuarios'
+        })
+    }
+
+}
 
 
 
 // Registro de usuarios
-const register = (req, res)=>{
+const register = async (req, res)=>{
 
     // Recogemos datos de la peticion
     let parametros = req.body
@@ -26,34 +47,54 @@ const register = (req, res)=>{
     let userToSave = new User(parametros)
 
     // control usuarios duplicados (email y nick)
-    User.find({
-        $or : [
-            {email : userToSave.email.toLowerCase() },
-            {nick : userToSave.nick.toLowerCase() } 
-        ]
-    }).exec( (error, users) =>{
-        if(error) return res.status(500).json({status:'error', msj:'error en la consulta'})
-        
-        if(users && users.length >= 1 ){
+    try {
+        const usuarios = await User.find({
+            // $or : [
+            //     {email : userToSave.email.toLowerCase() },
+            //     {nick : userToSave.nick.toLowerCase()}
+            // ]
+            $or : [
+                {email : userToSave.email },
+                {nick : userToSave.nick}
+             
+            ]
+        })
+
+
+        if(usuarios && usuarios.length >=1 ){
             return res.status(200).json({
                 status : 'success',
                 msj : 'El usuario ya existe..'
             })
         }
-        // cifrar la contraseña
 
+
+        // ciframos la contraseña
+        const hashPassword = await bcrypt.hash(userToSave.password, 10)
+        userToSave.password = hashPassword
+        
+        // Guardamos el usuario en la BBDD.
+        const newUserCifrado = new User(userToSave);
+        await newUserCifrado.save();  // guardamos a la BD mongo
+        
         // devolver resultados
-
         return res.status(200).json({
             status : 'success',
-            msj : 'Metodo de registro de usuarios',
-            userToSave
+            msj : 'Usuario registrado correctamente',
+            user : newUserCifrado
         })
-    })
+
+    } catch (error) {
+        return res.status(500).json({
+            status:'error', 
+            msj:'error en la consulta'
+        })
+        
+    }
 
 
 }
 
 export {
-    pruebaUser, register
+    pruebaUser, getAllUsers, register
 }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import express, { Router } from 'express'
-import {pruebaUser, register} from '../controllers/user.js'
+import {pruebaUser, getAllUsers, register} from '../controllers/user.js'
 
 const router = express.Router()
 
 // Definimos las rutas
 router.get('/prueba-usuario', pruebaUser)
+router.get('/getAllUsers', getAllUsers)
 router.post('/register', register)
 
 
